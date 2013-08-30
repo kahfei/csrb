9ce21991dd5c4df577f1df2cabfa721847526f65
kahfei <bak.kahfei@gmail.com>
2013-08-30 17:32:09 +0800

    linear equation progress

--- chap_4/lineareq.rb
+++ chap_4/lineareq.rb
@@ -1,4 +1,5 @@
-equations = ["y = 4x + 8","5y = 3x + 20"]
+equations = ["y = 4x + 8",
+			"5y = 3x + 20"]
 
 
 def getElements(eq)
@@ -9,10 +10,22 @@ def getElements(eq)
   end
   p elements
 end
-
   
-getElements("y + 4x + 8")
+#getElements("y = 4x + 8")
+
+def prepare(eq)
+  left = eq.delete(' ').split("=")[0]
+  right = eq.delete(' ').split("=")[1]
+
+  #if left[/\+|\-/]
+
+
+
+end
+
+prepare("y = 4x + 8")
 
 
 # http://www.mathsisfun.com/algebra/systems-linear-equations-matrices.html
 # https://www.khanacademy.org/math/algebra/algebra-matrices/inverting_matrices/v/matrices-to-solve-a-system-of-equations
+# http://stackoverflow.com/questions/10577783/ruby-checking-if-a-string-can-be-converted-to-an-integer
