316257c7f70d366c9a924721388169c658703f2f
kahfei <bak.kahfei@gmail.com>
2013-08-30 14:08:23 +0800

    update linear equation solution

--- chap_4/lineareq.rb
+++ chap_4/lineareq.rb
@@ -1,5 +1,18 @@
-require 'matrix'
-matrix = Matrix[[Rational(1), Rational(2)],[Rational(2),Rational(3)]]
-y = matrix.inverse
-puts matrix
-puts y
+equations = ["y = 4x + 8","5y = 3x + 20"]
+
+
+def getElements(eq)
+  elements = []
+  eq.scan(/\d*[a-zA-Z]/).each do |element|
+  	element = "1" << element unless element[/\d/]
+  	elements << element
+  end
+  p elements
+end
+
+  
+getElements("y + 4x + 8")
+
+
+# http://www.mathsisfun.com/algebra/systems-linear-equations-matrices.html
+# https://www.khanacademy.org/math/algebra/algebra-matrices/inverting_matrices/v/matrices-to-solve-a-system-of-equations
