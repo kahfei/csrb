5630d18ff8bb706d9e6b5c7da200601fc554c0b2
kahfei <bak.kahfei@gmail.com>
2013-09-09 06:18:06 +0800

    new linear eq

---
+++ chap_4/linear.rb
@@ -0,0 +1,30 @@
+equation = "3y = 4x + 8"
+
+
+def get_constant_result(eq)
+	eq.scan(/[\+|\-]\d+\b/)
+end
+
+def get_elements(eq)
+	eq.delete(" ").scan(/[\+|\-]*\d*[a-zA-Z]|\=|[\+|\-]*\d+/)
+end
+
+def add_coe(eq)
+	elements = []
+	eq.each do |item|
+		if item[/[\d+\b]|\d+[a-zA-Z]|\=/]
+			elements << add_sign(item)
+		else item[/[a-zA-Z]/]
+			elements << ("1" + add_sign(item))
+		end
+	end
+	elements
+end
+
+def add_sign(string)
+	if string[/^[\+|\-]/]
+		string
+	else
+		"+" + string
+	end
+end
